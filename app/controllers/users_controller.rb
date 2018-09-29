1dbe8e9afd09f996ec5163e46ea860dc8eb0c522
Walter Donnellan <Walt@Walters-MacBook-Pro.local>
2018-09-28 22:37:12 -0400

    user role defaults to member on create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.role.nil?
+      @user.role = "member"
+    end
     if @user.save
       session[:user_id] = @user.id
       redirect_to home_path, notice: "Thank you for signing up!"
