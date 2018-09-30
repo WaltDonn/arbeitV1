29364f9bd0a82caae7765f0ddc565cf84cd250e4
Walter Donnellan <Walt@Walters-MacBook-Pro.local>
2018-09-29 21:14:48 -0400

    removed file upload/download for tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180625114631) do
+ActiveRecord::Schema.define(version: 20180603003749) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer "project_id"
