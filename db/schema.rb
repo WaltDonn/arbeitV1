e757911198b70e26595d52dfaf8fb6cdf6b8ed05
Walter Donnellan <Walt@Walters-MacBook-Pro.local>
2018-09-29 21:50:41 -0400

    forgot to remove specsheet from schema

--- db/schema.rb
+++ db/schema.rb
@@ -49,8 +49,6 @@ ActiveRecord::Schema.define(version: 20180603003749) do
     t.integer "priority"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "specsheet"
-    t.string "specsheet_name"
   end
 
   create_table "users", force: :cascade do |t|
